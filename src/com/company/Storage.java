a5f64cb2f90fcd190f3ec17f4cc2be259ca87fc6
sergey2010 <ksd921020@yandex.ru>
2017-04-12 20:31:58 +0300

    Srez final

---
+++ src/com/company/Storage.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Storage {
+
+    private ArrayList<Long> list = new ArrayList<>();
+    private volatile boolean stop;
+
+    public ArrayList<Long> getList() {
+        return list;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+}
